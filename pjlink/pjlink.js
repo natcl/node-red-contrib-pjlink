07a4dae457bc531a4ab3c2b8ec98fac1eb0fc6db
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2015-02-03 17:06:20 -0500

    First working commit

--- pjlink/pjlink.js
+++ pjlink/pjlink.js
@@ -1,1 +1,21 @@
-//
+module.exports = function(RED) {  
+    var pjlink = require('pjlink');
+    function PJLink(config) {
+        RED.nodes.createNode(this,config);
+        this.ip = config.ip;
+        this.port = config.port;
+        this.password = config.password;
+        var node = this;
+        var beamer = new pjlink(node.ip, node.port, node.password);
+        this.on('input', function(msg) {
+            if (msg.payload == "on") {
+            	beamer.powerOn();
+            }
+            if (msg.payload == "off") {
+            	beamer.powerOff();
+            }
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("pjlink",PJLink);
+}
